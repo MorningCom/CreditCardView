f2fcc9b6305bddf6f814ce095393070c0fd5c9d6
Guilhem de Miollis <gdemiollis@gmail.com>
2016-11-30 12:42:04 +0100

    Second test avec Robolectric

--- creditcarddesign/src/test/java/com/cooltechworks/creditcarddesign/CardEditActivityTest.java
+++ creditcarddesign/src/test/java/com/cooltechworks/creditcarddesign/CardEditActivityTest.java
@@ -1,16 +1,32 @@
 package com.cooltechworks.creditcarddesign;
 
+
 import org.junit.Test;
+
+import android.os.Build;
+import org.junit.runner.RunWith;
+import org.robolectric.RobolectricTestRunner;
+import org.robolectric.RuntimeEnvironment;
+import org.robolectric.annotation.Config;
 
 import static org.junit.Assert.assertEquals;
 
+@RunWith(RobolectricTestRunner.class)
+@Config(constants = BuildConfig.class, sdk = Build.VERSION_CODES.M)
 public class CardEditActivityTest {
+
+    @Test
+    public void getNextButtonTextId() throws Exception {
+        CardEditActivity cardEditActivity = new CardEditActivity();
+        assertEquals(R.string.next, cardEditActivity.getNextButtonTextRes(2, 0));
+        assertEquals(R.string.done, cardEditActivity.getNextButtonTextRes(2, 1));
+    }
 
     @Test
     public void getNextButtonText() throws Exception {
         CardEditActivity cardEditActivity = new CardEditActivity();
-        assertEquals(R.string.next, cardEditActivity.getNextButtonTextRes(2, 0));
-        assertEquals(R.string.done, cardEditActivity.getNextButtonTextRes(2, 1));
+        assertEquals("NEXT", RuntimeEnvironment.application.getString(cardEditActivity.getNextButtonTextRes(2, 0)));
+        assertEquals("DONE", RuntimeEnvironment.application.getString(cardEditActivity.getNextButtonTextRes(2, 1)));
     }
 
 }
